25525cb407ec7ecf261dbaeeccbad7bf705acfd1
Daniel Galdino <danoligal@gmail.com>
2023-07-11 19:37:53 -0300

    commit inicial

---
+++ resources/lang/pt_BR/validation.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    "required" => "O campo :attribute é obrigatorio",
+    "date" => "O campo : attribute precisa ser uma data válida",
+    "string" => "O campo :attribute precias ser um texto",
+    "exists" => "O campo :attribute nao existe no sistema",
+    "after" => "O campo :attribute precisa ser uma data válida",
+    "integer" => "O campo :attribute precisa ser um número inteiro",
+    "unique" => "O campo :attribute precisa ser único"
+];
